231689726f58f27176d5a0943b002fb348f3c461
fenogljc <fenogljc@rose-hulman.edu>
2017-01-13 14:06:46 -0500

    Added more tests for the modeler

---
+++ src/test/java/DriverTests/ModelerTest.java
@@ -0,0 +1,54 @@
+package DriverTests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Drivers.Modeler;
+import ModelObjects.ModelObject;
+import ModelObjects.OneToOneDependency;
+import ModelObjects.UMLInterface;
+
+public class ModelerTest {
+
+	Modeler m;
+	
+	@Before
+	public void setUp() throws Exception {
+		m = new Modeler();
+	}
+
+	@Test
+	public void testCreateClassModelsInterface() throws Exception {
+		List<String> classes = new ArrayList<String>();
+		classes.add("Runners.Runner");
+		m.createClassModels(classes);
+		
+		List<ModelObject> models = m.getObjects();
+		assertFalse(models.isEmpty());
+		assertEquals("Runner", models.get(0).getName());
+		assertEquals(models.get(0).getClass(), UMLInterface.class);
+		assertEquals(models.get(1).getClass(), OneToOneDependency.class);
+	}
+	
+	@Test
+	public void testCreateClassModelsRecursion() throws Exception {
+		m.setRecursion(true);
+		List<String> classes = new ArrayList<String>();
+		classes.add("Parsers.HTMLClassParser");
+		m.createClassModels(classes);
+		
+		List<ModelObject> models = m.getObjects();
+		assertEquals(9, models.size());
+		
+		
+		
+	}
+	
+	
+
+}
