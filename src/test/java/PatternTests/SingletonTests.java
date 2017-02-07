19831420e2f64e7e2e66ba65a973e65e53ac0e98
fenogljc <fenogljc@rose-hulman.edu>
2017-02-07 00:07:50 -0500

    literally just an empty test file for now.

---
+++ src/test/java/PatternTests/SingletonTests.java
@@ -0,0 +1,20 @@
+package PatternTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SingletonTests {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
